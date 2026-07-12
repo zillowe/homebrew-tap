3d2f7108ba4cc0fa4d11647edc6bedf4440d4050
Zillowe CI/CD <contact@zillowe.qzz.io>
2026-07-12 11:15:49 +0000

    Release: 1.22.1

--- zoi.rb
+++ zoi.rb
@@ -3,29 +3,29 @@
 class Zoi < Formula
   desc "Advanced Package Manager & Environment Orchestrator"
   homepage "https://gitlab.com/zillowe/zillwen/zusty/zoi"
-  version "1.21.1"
+  version "1.22.1"
   license "Apache-2.0"
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://gitlab.com/zillowe/zillwen/zusty/zoi/-/releases/Prod-Release-#{version}/downloads/zoi-macos-arm64.tar.zst"
-      sha256 "e2b5e92a6b9aaf6fd1891a0c82ec0511a994368ba5eb09942404238c4e5baa88"
+      sha256 "816a9988d7f180f20b9e862da919fd4c49d7553a18c6b3f4203f983dc18cff90"
     end
 
     if Hardware::CPU.intel?
       url "https://gitlab.com/zillowe/zillwen/zusty/zoi/-/releases/Prod-Release-#{version}/downloads/zoi-macos-amd64.tar.zst"
-      sha256 "7461ae917c1340a274d7fcee09f338bc9495a6d27696a7b4cc3570862ec9c5e8"
+      sha256 "aa677e9a15f24cadda9385a072594bb1ae1a7380352a6b95d5e6936385f48b4c"
     end
 
     on_linux do
       if Hardware::CPU.intel? and Hardware::CPU.is_64_bit?
         url "https://gitlab.com/zillowe/zillwen/zusty/zoi/-/releases/Prod-Release-#{version}/downloads/zoi-linux-amd64.tar.zst"
-        sha256 "ef28c6f318b8913db9b27f8fea4948a2f990faba2e00a58270be190ff10eb4b5"
+        sha256 "832c01a7abb580d8d899b12c34dfc68dd3f9f5abbadd97504706d820a24d0a03"
       end
 
       if Hardware::CPU.arm? and Hardware::CPU.is_64_bit?
         url "https://gitlab.com/zillowe/zillwen/zusty/zoi/-/releases/Prod-Release-#{version}/downloads/zoi-linux-arm64.tar.zst"
-        sha256 "c6a2b39811d79cdf8b0212294d35ac270284d2ca03b915f7828c1d7b0b3c42e3"
+        sha256 "8faf205606291da516de10dcabf42e67a70eee955596701689618144fb975b1a"
       end
     end
 
@@ -36,5 +36,8 @@ class Zoi < Formula
     (bash_completion/"zoi").write `#{bin}/zoi generate-completions bash`
     (zsh_completion/"_zoi").write `#{bin}/zoi generate-completions zsh`
     (fish_completion/"zoi.fish").write `#{bin}/zoi generate-completions fish`
+
+    system "#{bin}/zoi", "generate-manual"
+    man1.install Dir["manuals/*.1"]
   end
 end
